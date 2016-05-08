3c3a8f9b3b647a55c531aa73ac5c14d5a073ad7d
Danny Andres <danny.aguilar@usach.cl>
2016-05-08 13:01:10 -0300

    cambio del for
    cambie el for y los if, elseif 
    por un foreach para reconocer las variables (eventos)
    tambien comente mas el codigo

--- github_score.php
+++ github_score.php
@@ -1,24 +1,13 @@
 <?php
 
-  	function valid_event($evento){
-		$arrayevents=["PushEvent","CreateEvent","IssuesEvent","CommitCommentEvent"];
-		for ($i=0; $i < count($arrayevents); $i++) { 
-			if ($evento == $arrayevents[i]) {
-				return true;
-			}
-		}
-		return false;
-	}
+ /*-----------------------OBTENER EL JSON DESDE LA URL Y ASIGNANDOLE UNA VARIABLE------------------------------------*/
+
+ 	$user=$_POST["user"];
+	$url="https://api.github.com/users/".$user."/events";
+	$url_profile="https://api.github.com/users/".$user;
 
 
-/*-----------------------OBTENER EL JSON DESDE LA URL Y ASIGNANDOLE UNA VARIABLE------------------------------------*/
-
-  	$user=$_POST["user"];
-  	$url="https://api.github.com/users/".$user."/events";
-  	$url_profile="https://api.github.com/users/".$user;
-  
-  
-  	$ch=curl_init();                                       //inicio el curl
+	$ch=curl_init();                                       //inicio el curl
 	curl_setopt($ch, CURLOPT_URL, $url);                   //fijo la url
 	curl_setopt($ch, CURLOPT_USERAGENT, 'DannyAndres');    //user, para acceder a github
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);        //si el user existe te muestra info.
@@ -27,7 +16,7 @@
 	$curl_response=curl_exec($ch);                         /*curl exec ejecuta la peticion html asignandole una   
                                                            variable..... la peticion html es el json que obtenemos*/
 
-	$json=json_decode($curl_response);                     //codifico el json(array) que antes nombramos curl response
+	$json=json_decode($curl_response, true);                     //codifico el json(array) que antes nombramos curl response
 
 	curl_close($ch);                                       //cierro el curl
 
@@ -41,30 +30,52 @@
 
 	$puntaje=0;                    //puntaje total
 
+	/*
+	PushEvent: Otorga 5
+	CreateEvent: Otorga 4
+	IssuesEvent: Otorga 3
+	CommitCommentEvent: Otorga 2
+	otro: Otorga 1
+	*/
+
 /*-------------------------------------------------------------------------------------------------------------------*/
 
-	for ($i=0; $i<count($json) ; $i++) { 
-		
-		if ($json[$i]->type == "PushEvent") {
-			$puntaje=$puntaje+5;
+	foreach ($json as $var) {
+		if($var['type'] == "PushEvent"){
+			$puntaje = $puntaje + 5;
 			$push=$push+1;
 		}
-		elseif ($json[$i]->type == "CreateEvent") {
-			$puntaje=$puntaje+4;
+		elseif ($var['type'] == "CreateEvent") {
+			$puntaje = $puntaje + 4;
 			$create=$create+1;
 		}
-		elseif ($json[$i]->type == "IssuesEvent") {
-			$puntaje=$puntaje+3;
+		elseif ($var['type'] == "IssuesEvent") {
+			$puntaje = $puntaje + 3;
 			$issues=$issues+1;
 		}
-		elseif ($json[$i]->type == "CommitCommentEvent") {
-			$puntaje=$puntaje+2;
+		elseif ($var['type'] == "CommitCommentEvent") {
+			$puntaje = $puntaje + 2;
 			$commit=$commit+1;
 		}
-		elseif (!valid_event($json[$i]->type)) {
-			$puntaje+$puntaje+1;
+		else {
+			$puntaje = $puntaje + 1;
 			$otro=$otro+1;
 		}
 	}
 
+	echo "GitHub Score: "." ".$puntaje."<br>";
+	echo "Eventos:"."<br>";
+	echo "PushEvent:"." ".$push."<br>";
+	echo "CreateEvent:"." ".$create."<br>";
+	echo "IssuesEvent:"." ".$issues."<br>";
+	echo "CommitCommentEvent:"." ".$commit."<br>";
+	echo "Otro:"." ".$otro."<br>";
+	
+
+	/*PushEvent: Otorga 5
+	CreateEvent: Otorga 4
+	IssuesEvent: Otorga 3
+	CommitCommentEvent: Otorga 2
+	otro: Otorga 1
+	*/ 
 ?>
